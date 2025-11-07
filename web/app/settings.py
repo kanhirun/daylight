a7d50987c49471087c0693ed89f1eeb8c6aafe52
Kel Kanhirun <kel@native.dev>
2025-11-07 10:31:18 -0500

    Installs graphene_django for GraphQL support
    Adds stubbed graphql endpoint

--- web/app/settings.py
+++ web/app/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', # Required for GraphiQL
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "app.schema.schema"
+}
