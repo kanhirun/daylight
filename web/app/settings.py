b091a7f78ee2a2d9a050376c6991979afbe636fc
Kel Kanhirun <kel@native.dev>
2025-11-07 15:46:41 -0500

    Adds django-graphql-jwt for JWT support

--- web/app/settings.py
+++ web/app/settings.py
@@ -130,5 +130,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
-    "SCHEMA": "app.schema.schema"
+    "SCHEMA": "app.schema.schema",
+    "MIDDLEWARE": [
+        "graphql_jwt.middleware.JSONWebTokenMiddleware",
+    ]
 }
+
+AUTHENTICATION_BACKENDS = [
+    "graphql_jwt.backends.JSONWebTokenBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
